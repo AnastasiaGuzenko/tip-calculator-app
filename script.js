eca8d2c774890959bd4c4f3b15a03405cf625eb1
Anastasia <guzenkoanvl@gmail.com>
2022-07-04 17:08:27 +0300

    add active view

---
+++ script.js
@@ -0,0 +1,15 @@
+// const btns = document.querySelectorAll('.btn')
+
+// btns.forEach(btn => {
+//   btn.addEventListener('click', function (e) {
+
+//     e.target.style.backgroundColor = "#26C2AD";
+//   });
+// });
+
+
+
+function changeColor(elem) {
+  elem.style.backgroundColor = '#F3F8FB'
+  elem.style.border = '2px solid #5CA99F'
+}
