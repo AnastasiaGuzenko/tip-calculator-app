14fb11342bc7af1643151621cc5fed580d685987
Anastasia <guzenkoanvl@gmail.com>
2022-07-05 12:30:41 +0300

    add calc all

--- script.js
+++ script.js
@@ -1,35 +1,63 @@
-let btns = document.querySelectorAll('.btn');
 let btnIndex;
+let btnTip = 15;
+let btns = document.querySelectorAll('.btn');
 let btnReset = document.querySelector('.btn_big');
-
-let inputBill = document.querySelector('.bill')
-let inputTip  = document.querySelector('.btn_input')
+let inputBill = document.querySelector('.bill');
+let inputTip  = document.querySelector('.btn_input');
 let inputPeople  = document.querySelector('.number-of-people');
-
-let tipStr = document.querySelector('.tip')
-let totalStr = document.querySelector('.total')
-
-let inputs = document.querySelectorAll('input')
+let tipStr = document.querySelector('.tip');
+let totalStr = document.querySelector('.total');
+let inputs = document.querySelectorAll('input');
+let bill;
+let tip;
+let people;
 
 function showResultCalc(bill = 8, tip = 15, people = 1) {
   let tipSum = (bill / 100 * tip)
   let resultTip =  (tipSum / people).toFixed(2)
   let resultTotal = ((Number(bill) + Number(tipSum)) / people).toFixed(2)
 
-  tipStr.innerHTML = ` $${resultTip}`
-  totalStr.innerHTML = ` $${resultTotal}`
+  if (isNaN(resultTip) == true) {
+    tipStr.innerHTML = `$0`
+  } else {
+    tipStr.innerHTML = `$${resultTip}`
+  }
+
+  if (isNaN(resultTotal) == true) {
+    totalStr.innerHTML = `$0`
+  } else {
+    totalStr.innerHTML = `$${resultTotal}`
+  }
 }
 
-showResultCalc()
+showResultCalc(inputBill.value, btnTip, inputPeople.value)
 
 for (let i = 0; i < inputs.length; i++) {
-  inputs[i].addEventListener('change', function() {
-    let bill = inputBill.value
-    let tip = inputTip.value
-    let people = inputPeople.value
+  inputs[i].addEventListener('blur', function() {
+    bill = inputBill.value
+    
+    if (inputTip.value == '') {
+      tip = btnTip
+    } else {
+      tip = inputTip.value
+    }
   
+    people = inputPeople.value
     showResultCalc(bill, tip, people)
   })
+}
+
+function btnClick() {
+  for (let i = 0; i < btns.length; i++) {
+    if (i == btnIndex) {
+      btns[i].classList.add('btn_active')
+      btnTip = parseInt((btns[i].innerHTML), 10)
+    } else {
+      btns[i].classList.remove('btn_active')
+    }
+
+    showResultCalc(bill, btnTip, people)
+  }
 }
 
 for (let i = 0; i < btns.length; i++) {
@@ -41,17 +69,6 @@ for (let i = 0; i < btns.length; i++) {
     }
     btnClick()
   })
-  
-}
-
-function btnClick() {
-  for (let i = 0; i < btns.length; i++) {
-    if (i == btnIndex) {
-      btns[i].classList.add('btn_active')
-    } else {
-      btns[i].classList.remove('btn_active')
-    }
-  }
 }
 
 function changeColor(elem) {
@@ -59,10 +76,10 @@ function changeColor(elem) {
   elem.style.border = '2px solid #5CA99F'
 }
 
-btnReset.addEventListener('click', function() {
-  inputBill.value = ''
-  inputTip.innerHTML = ''
-  inputPeople.value = ''
-})
-
-
+// btnReset.addEventListener('click', function() {
+//   inputBill.value = ''
+//   inputTip.innerHTML = ''
+//   inputPeople.value = ''
+//   tipStr.innerHTML = `$0`
+//   totalStr.innerHTML = `$0`
+// })
